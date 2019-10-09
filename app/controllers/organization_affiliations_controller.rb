7d270448cf3f0b5662c574ae3913c22e75c0276d
Stephen MacVicar <smacvicar@mitre.org>
2019-10-09 12:51:51 -0400

    update OrganizationAffiliation search

--- app/controllers/organization_affiliations_controller.rb
+++ app/controllers/organization_affiliations_controller.rb
@@ -7,7 +7,7 @@
 ################################################################################
 
 require 'json'
-	
+
 class OrganizationAffiliationsController < ApplicationController
 
 	before_action :connect_to_server, only: [ :index, :show ]
@@ -29,6 +29,7 @@ class OrganizationAffiliationsController < ApplicationController
 			@@bundle = reply.resource
 		end
 
+    @query_params = query_params
 		@organization_affiliations = @@bundle.entry.map(&:resource)
 	end
 
@@ -41,4 +42,76 @@ class OrganizationAffiliationsController < ApplicationController
 											search: { parameters: { id: params[:id] } })
 	end
 
+  def query_params
+    [
+      {
+        name: 'Active',
+        value: 'active'
+      },
+      {
+        name: 'Date',
+        value: 'date'
+      },
+      {
+        name: 'Email',
+        value: 'email'
+      },
+      {
+        name: 'Endpoint',
+        value: 'endpoint'
+      },
+      {
+        name: 'ID',
+        value: '_id'
+      },
+      {
+        name: 'Identifier',
+        value: 'identifier'
+      },
+      {
+        name: 'Identifier Assigner',
+        value: 'identifier-assigner'
+      },
+      {
+        name: 'Intermediary',
+        value: 'via-intermediary'
+      },
+      {
+        name: 'Location',
+        value: 'location'
+      },
+      {
+        name: 'Network',
+        value: 'network'
+      },
+      {
+        name: 'Participating Organization',
+        value: 'participating-organization'
+      },
+      {
+        name: 'Phone',
+        value: 'phone'
+      },
+      {
+        name: 'Primary Organization',
+        value: 'primary-organization'
+      },
+      {
+        name: 'Role',
+        value: 'role'
+      },
+      {
+        name: 'Service',
+        value: 'service'
+      },
+      {
+        name: 'Specialty',
+        value: 'specialty'
+      },
+      {
+        name: 'Telecom',
+        value: 'telecom'
+      }
+    ]
+  end
 end
