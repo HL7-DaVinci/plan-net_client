977e215952d706e9725be3d1d1c3a061dad98ff9
Stephen MacVicar <smacvicar@mitre.org>
2019-10-09 12:27:16 -0400

    update Location search

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,7 +7,7 @@
 ################################################################################
 
 require 'json'
-	
+
 class LocationsController < ApplicationController
 
 	before_action :connect_to_server, only: [ :index, :show ]
@@ -29,6 +29,7 @@ class LocationsController < ApplicationController
 			@@bundle = reply.resource
 		end
 
+    @query_params = query_params
 		@locations = @@bundle.entry.map(&:resource)
 	end
 
@@ -41,8 +42,120 @@ class LocationsController < ApplicationController
 											search: { parameters: { _id: params[:id] } })
 		bundle = reply.resource
 		fhir_location = bundle.entry.map(&:resource).first
-		
+
 		@location = Location.new(fhir_location) unless fhir_location.nil?
 	end
 
+  def query_params
+    [
+      {
+        name: 'ID',
+        value: '_id'
+      },
+      {
+        name: 'Accessibility',
+        value: 'accessibility'
+      },
+      {
+        name: 'Address',
+        value: 'address'
+      },
+      {
+        name: 'Available Days',
+        value: 'available-days'
+      },
+      {
+        name: 'Available End Time',
+        value: 'available-endtime'
+      },
+      {
+        name: 'Available Start Time',
+        value: 'available-start-time'
+      },
+      {
+        name: 'City',
+        value: 'address-city'
+      },
+      {
+        name: 'Contains',
+        value: 'contains'
+      },
+      {
+        name: 'Country',
+        value: 'address-country'
+      },
+      {
+        name: 'endpoint',
+        value: 'Endpoint'
+      },
+      {
+        name: 'Identifier',
+        value: 'identifier'
+      },
+      {
+        name: 'Intermediary',
+        value: 'via-intermediary'
+      },
+      {
+        name: 'Identifier Assigner',
+        value: 'identifier-assigner'
+      },
+      {
+        name: 'New Patient Network',
+        value: 'new-patient-network'
+      },
+      {
+        name: 'Near',
+        value: 'near'
+      },
+      {
+        name: 'New Patient',
+        value: 'new-patient'
+      },
+      {
+        name: 'Operational Status',
+        value: 'operational-status'
+      },
+      {
+        name: 'Organization',
+        value: 'organization'
+      },
+      {
+        name: 'Part of',
+        value: 'partof'
+      },
+      {
+        name: 'Postal Code',
+        value: 'address-postalcode'
+      },
+      {
+        name: 'State',
+        value: 'address-state'
+      },
+      {
+        name: 'Status',
+        value: 'status'
+      },
+      {
+        name: 'Telecom Available Days',
+        value: 'telecom-available-days'
+      },
+      {
+        name: 'Telecom Available End Time',
+        value: 'telecom-available-endtime'
+      },
+      {
+        name: 'Telecom Available Start Time',
+        value: 'telecom-available-start-time'
+      },
+      {
+        name: 'Type',
+        value: 'type'
+      },
+      {
+        name: 'Use',
+        value: 'address-use'
+      }
+    ]
+  end
 end
