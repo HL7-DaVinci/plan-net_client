83b8b8fd3bb8e5a345c5ce96435551c98195aedb
Preston Lee <preston.lee@prestonlee.com>
2020-09-10 11:16:45 -0700

    Switching to SQLite and auto-migrating and auto-seeding on container startup.

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require_relative 'boot'
 require 'rails/all'
 require 'openssl'
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -18,5 +18,6 @@ module FormularyClient
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_08_13_134506) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "zipcodes", force: :cascade do |t|
     t.string "zip"
     t.string "city"
